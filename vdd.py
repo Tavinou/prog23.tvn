779917613fc4011a3b51afb8f30fcf73eb7ed653
tavin <gustavoh.roepke@gmail.com>
2023-02-16 11:03:33 -0300

    diabo

--- vdd.py
+++ vdd.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def oi():
-    return "<b> baleia 2 </b>"
-app.run(port=4999)
+    return "<b> blau blau </b>"
+app.run(debug=True, host="0.0.0.0",port=4999)
