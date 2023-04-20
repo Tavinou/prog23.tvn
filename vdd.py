be3cc4c02eb9221eedbff8f6c936c024038dea5f
__global <__global>
2023-04-20 13:48:47 -0300

    da o cu

--- vdd.py
+++ vdd.py
@@ -1,7 +1,13 @@
 from flask import Flask 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/pagina um")
 def oi():
-    return "<b> blau blau </b>"
+    return "<b>EVANIR PFLEGER</b>"
+
+@app. route("/paginadois")
+def link():
+    return "<a href=pagina um <clika aq></a>"
+
+
 app.run(debug=True, host="0.0.0.0",port=4999)
